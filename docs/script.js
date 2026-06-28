afe76d1a7f60d38238a9414b0bb6b2a9f678569a
Amylylyte <lushymid@gmail.com>
2026-06-28 01:46:01 -0400

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,41 @@
-
+let runs = 0;
+let HC1 = 0;
+let HC2 = 0;
+let TC1 = 0;
+let TC1point5 = 0;
+let TC2 = 0;
+let TC2point5 = 0;
+let HC3 = 0;
+let HC4 = 0;
+let TC3 = 0;
+let TC3point5 = 0;
+let IMP1 = 0;
+let IMP2 = 0;
+let TC4 = 0;
+let TC4point5 = 0;
+let BREAK = 0;
+let B1 = 0;
+let B1point5 = 0;
+let B2 = 0;
+let B2point5 = 0;
+let RAY1 = 0;
+let RAY1point5 = 0;
+let RAY2 = 0;
+let RAY2point5 = 0;
+let RAY3 = 0;
+let SLOW1 = 0;
+let SLOW2 = 0;
+let PENG = 0;
+let SWEEP = 0;
+let FC1 = 0;
+let FC1point5 = 0;
+let FC2 = 0;
+let FC1point5 = 0;
+let FC3 = 0;
+const SectionName = {
+    0: "Hard Chords 1", 1: "Hard Chords 2", 2: "Tricky Chords 1", 3: "Tricky Chords 1.5", 4: "Tricky Chords 2", 5: "Tricky Chords 2.5", 6: "Hard Chords 3", 
+  7: "Hard Chords 4", 8: "Tricky Chords 3", 9: "Tricky Chords 3.5", 10: "Impossible 1", 11: "Impossible 2", 12: "Tricky Chords 4", 13: "Tricky Chords 4.5",
+  14: "Break", 15: "Blooming 1", 16: "Blooming 1.5", 17: "Blooming 2", 18: "Blooming 2.5", 19: "Rays 1", 20: "Rays 1.5", 21: "Rays 2", 22: "Rays 2.5", 
+  23: "Rays 3", 24: "Slow Melody 1", 25: "Slow Melody 2", 26: "Not Penger", 27: "Slow Sweeps", 28: "Final Chords 1", 29: "Final Chords 1.5", 
+  30: "Final Chords 2", 31: "Final Chords 2.5", 32: "Final Chords 3"
+};
